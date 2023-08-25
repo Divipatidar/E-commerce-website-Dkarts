66b5ac5b183ec68b611942865b9c3eb2be02f936
Divipatidar <102014528+Divipatidar@users.noreply.github.com>
2023-08-25 12:17:25 +0530

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL:"http://127.0.0.1:5001/e-clone-d2cad/us-central1/api"
+    baseURL:""
 })
 export default instance;
 // http://127.0.0.1:5001/e-clone-d2cad/us-central1/api
